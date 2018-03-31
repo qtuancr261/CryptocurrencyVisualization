cc2d77162ff8906f09a2806576d3d25e29ed2b92
qtuancr261 <tuancr261@gmail.com>
2018-03-31 22:20:34 +0700

    add DataPublisher Interface that represent publisher object in Observer Pattern

--- Publisher/datapublisher.h
+++ Publisher/datapublisher.h
@@ -1,4 +1,10 @@
 #ifndef DATAPUBLISHER_H
 #define DATAPUBLISHER_H
-
+#include "Observer/dataobserver.h"
+class DataPublisher
+{
+    virtual void registerObserver(const DataObserver& observer) = 0;
+    virtual void removeObserver(const DataObserver& observer) = 0;
+    virtual void notifyAllObservers() = 0;
+};
 #endif // DATAPUBLISHER_H
