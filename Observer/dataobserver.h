4632df45db97a6b08cca2d480e3be3cc0e7bbef5
qtuancr261 <tuancr261@gmail.com>
2018-03-31 22:19:45 +0700

    add DataObserver Interface that represent observer object in Observer Pattern

--- Observer/dataobserver.h
+++ Observer/dataobserver.h
@@ -1,4 +1,9 @@
 #ifndef DATAOBSERVER_H
 #define DATAOBSERVER_H
-
+#include "Model/coin.h"
+#include <QVector>
+class DataObserver
+{
+    virtual void update(QVector<Coin*> coins) = 0;
+};
 #endif // DATAOBSERVER_H
