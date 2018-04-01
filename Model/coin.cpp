bf6a2a944d93dd1957fa53fde667884ab90dd81c
qtuancr261 <tuancr261@gmail.com>
2018-04-01 11:04:59 +0700

    Construct Coin class

--- Model/coin.cpp
+++ Model/coin.cpp
@@ -1,6 +1,52 @@
 #include "coin.h"
 
-Coin::Coin()
+Coin::Coin(QString name, QString symbol)
+    : name{name},
+      symbol{symbol},
+      available_supply{},
+      lastValue{}
 {
-
+    //
 }
+
+QString Coin::getName() const
+{
+    return name;
+}
+
+void Coin::setName(const QString &value)
+{
+    name = value;
+}
+
+QString Coin::getSymbol() const
+{
+    return symbol;
+}
+
+void Coin::setSymbol(const QString &value)
+{
+    symbol = value;
+}
+
+QIcon Coin::getIcon() const
+{
+    return icon;
+}
+
+void Coin::setIcon(const QIcon &value)
+{
+    icon = value;
+}
+
+long Coin::getAvailable_supply() const
+{
+    return available_supply;
+}
+
+void Coin::setAvailable_supply(long value)
+{
+    available_supply = value;
+}
+
+
--- Model/coin.h
+++ Model/coin.h
@@ -1,10 +1,37 @@
 #ifndef COIN_H
 #define COIN_H
-
+#include <QString>
+#include <QIcon>
+#include <QVector>
+using ulong = unsigned long;
 class Coin
 {
+private:
+    QString name;
+    QString symbol;
+    QIcon icon;
+    long available_supply;
+    struct value
+    {
+        ulong timeStamp;
+        ulong price;
+        ulong marketcap;
+        double volume_24h;
+        double changedPercent_1h;
+        double changedPercent_24h;
+    };
+    value lastValue;
+    QVector<value> allValues;
 public:
-    Coin();
+    explicit Coin(QString name, QString symbol);
+    QString getName() const;
+    void setName(const QString &value);
+    QString getSymbol() const;
+    void setSymbol(const QString &value);
+    QIcon getIcon() const;
+    void setIcon(const QIcon &value);
+    long getAvailable_supply() const;
+    void setAvailable_supply(long value);
 };
 
 #endif // COIN_H
