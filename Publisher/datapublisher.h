f2d33dcd6405bc52fec81257940f95f101500e0d
qtuancr261 <tuancr261@gmail.com>
2018-04-02 22:56:58 +0700

    include c++14 header <memory> to use smart pointers

--- Publisher/datapublisher.h
+++ Publisher/datapublisher.h
@@ -1,10 +1,15 @@
 #ifndef DATAPUBLISHER_H
 #define DATAPUBLISHER_H
 #include "Observer/dataobserver.h"
+#include <memory>
+using std::shared_ptr;
+using std::make_shared;
+using std::weak_ptr;
+using DataObserverPtr = shared_ptr<DataObserver>;
 class DataPublisher
 {
-    virtual void registerObserver(const DataObserver& observer) = 0;
-    virtual void removeObserver(const DataObserver& observer) = 0;
+    virtual void registerObserver(const DataObserverPtr& observer) = 0;
+    virtual void removeObserver(const DataObserverPtr& observer) = 0;
     virtual void notifyAllObservers() = 0;
 };
 #endif // DATAPUBLISHER_H
