e1fed7d36862bde6571ec8fbc25a973adc12856b
qtuancr261 <tuancr261@gmail.com>
2018-04-02 22:51:58 +0700

    Temporarily used QVector for store Coins in Collection

--- Model/coincollection.h
+++ Model/coincollection.h
@@ -3,9 +3,12 @@
 
 #include <QObject>
 #include "Observer/dataobserver.h"
+#include "Model/coin.h"
 class CoinCollection : public QObject, public DataObserver
 {
     Q_OBJECT
+private:
+    QVector<Coin*> coins;
 public:
     explicit CoinCollection(QObject *parent = nullptr);
 
