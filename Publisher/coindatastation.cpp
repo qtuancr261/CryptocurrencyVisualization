2dc2061aff5ac83d2410bc110fb88138dc111e4b
qtuancr261 <tuancr261@gmail.com>
2018-04-02 22:56:05 +0700

    Use std::map to store all coin tracked from Cry WS Server | QNetworkAccessManager to send requests and receive replies using CryWSAPI

--- Publisher/coindatastation.cpp
+++ Publisher/coindatastation.cpp
@@ -1,6 +1,32 @@
 #include "coindatastation.h"
 
-CoinDataStation::CoinDataStation(QObject *parent) : QObject(parent)
+CoinDataStation::CoinDataStation(QObject *parent)
+    : QObject(parent),
+      networkManager{new QNetworkAccessManager(this)}
+{
+    QObject::connect(networkManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(gotLastValueOfALLCoins(QNetworkReply*)));
+}
+
+void CoinDataStation::getLastValueOfAllCoins()
+{
+    networkManager->get(QNetworkRequest(QUrl(API.getGetDataWithoutAllValues())));
+}
+
+void CoinDataStation::gotLastValueOfALLCoins(QNetworkReply *replyFromServer)
+{
+    qDebug() << replyFromServer->readAll();
+}
+
+
+void CoinDataStation::registerObserver(const DataObserverPtr &observer)
 {
 
+}
+
+void CoinDataStation::removeObserver(const DataObserverPtr &observer)
+{
+}
+
+void CoinDataStation::notifyAllObservers()
+{
 }
--- Publisher/coindatastation.h
+++ Publisher/coindatastation.h
@@ -2,16 +2,35 @@
 #define COINDATASTATION_H
 
 #include <QObject>
-
-class CoinDataStation : public QObject
+#include <map>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QtDebug>
+#include "Publisher/datapublisher.h"
+#include "Model/crywsapi.h"
+using std::map;
+using CoinPtr = shared_ptr<Coin>;
+class CoinDataStation : public QObject, public DataPublisher
 {
     Q_OBJECT
+private:
+    map<QString, CoinPtr> trackedCoins;
+    QNetworkAccessManager* networkManager;
+    CryWSAPI API;
 public:
     explicit CoinDataStation(QObject *parent = nullptr);
 
 signals:
 
 public slots:
+    void getLastValueOfAllCoins();
+    void gotLastValueOfALLCoins(QNetworkReply* replyFromServer);
+    // DataPublisher interface
+public:
+    virtual void registerObserver(const DataObserverPtr &observer) override;
+    virtual void removeObserver(const DataObserverPtr &observer) override;
+    virtual void notifyAllObservers() override;
 };
 
 #endif // COINDATASTATION_H
