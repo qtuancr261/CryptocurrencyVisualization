cf8949c36f50e3bfac6787effad267fdcd794a33
qtuancr261 <tuancr261@gmail.com>
2018-04-05 01:52:09 +0700

    using QHash<QString, CoinPtr> to hold the coins we want to track

--- Model/coincollection.cpp
+++ Model/coincollection.cpp
@@ -1,5 +1,26 @@
 #include "coincollection.h"
 
+const QHash<QString, CoinPtr> &CoinCollection::getTrackedCoins() const
+{
+    return trackedCoins;
+}
+
+void CoinCollection::addNewTrackedCoin(const QString &coinSymbol, const CoinPtr &coin)
+{
+    if (!trackedCoins.contains(coinSymbol))
+        trackedCoins.insert(coinSymbol, coin);
+    else
+        qDebug() << "Duplicated Coin";
+}
+
+void CoinCollection::removeTrackedCoin(const QString &coinSymbol)
+{
+    if(trackedCoins.remove(coinSymbol))
+        qDebug() << "Remove Tracked Coin successfully ";
+    else
+        qDebug() << "Remove Tracked Coin failed ";
+}
+
 CoinCollection::CoinCollection(QObject *parent) : QObject(parent)
 {
 
--- Model/coincollection.h
+++ Model/coincollection.h
@@ -4,13 +4,18 @@
 #include <QObject>
 #include "Observer/dataobserver.h"
 #include "Model/coin.h"
+#include <QHash>
+#include "Publisher/coindatastation.h"
 class CoinCollection : public QObject, public DataObserver
 {
     Q_OBJECT
 private:
-    QVector<Coin*> coins;
+    QHash<QString, CoinPtr> trackedCoins;
 public:
     explicit CoinCollection(QObject *parent = nullptr);
+    const QHash<QString, CoinPtr>& getTrackedCoins() const;
+    void addNewTrackedCoin(const QString& coinSymbol, const CoinPtr& coin);
+    void removeTrackedCoin(const QString& coinSymbol);
 
 signals:
 
