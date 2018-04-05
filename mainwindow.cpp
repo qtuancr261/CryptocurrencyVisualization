83c3a523badd1e48cc6c29561fa680004ab09b52
qtuancr261 <tuancr261@gmail.com>
2018-04-05 15:43:29 +0700

    :3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,11 +10,30 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     dataStation->getLastValueOfAllCoins();
     QObject::connect(ui->toolButtonAddNewList, SIGNAL(clicked(bool)), collectionManagerDialog, SLOT(showAddNewCollectionCoin()));
+    QObject::connect(ui->toolButtonAddNewList, SIGNAL(clicked(bool)), this , SLOT(disableAllToolButtonRelativeWithCollectionList()));
+
+    QObject::connect(ui->toolButtonConfigList, SIGNAL(clicked(bool)), collectionManagerDialog, SLOT(showConfigureCurrentCollectionCoin()));
+    QObject::connect(ui->toolButtonConfigList, SIGNAL(clicked(bool)), this, SLOT(disableAllToolButtonRelativeWithCollectionList()));
     //QObject::connect(ui->toolButtonAddNewList, SIGNAL(clicked(bool)), dataStation, SLOT(getLastValueOfAllCoins()));
     QObject::connect(dataStation, SIGNAL(parseLastValueOfAllCoinsCompleted(QHash<QString,CoinPtr>)), collectionManagerDialog, SLOT(loadAvailableCoins(QHash<QString,CoinPtr>)));
+    QObject::connect(collectionManagerDialog, SIGNAL(finishedCurrentAction()), this, SLOT(enableAllToolButtonRelativeWithCollectionList()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::disableAllToolButtonRelativeWithCollectionList()
+{
+    ui->toolButtonAddNewList->setDisabled(true);
+    ui->toolButtonConfigList->setDisabled(true);
+    ui->toolButtonRemoveList->setDisabled(true);
+}
+
+void MainWindow::enableAllToolButtonRelativeWithCollectionList()
+{
+    ui->toolButtonAddNewList->setEnabled(true);
+    ui->toolButtonConfigList->setEnabled(true);
+    ui->toolButtonRemoveList->setEnabled(true);
 }
