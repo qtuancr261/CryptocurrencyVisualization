bd972a83160be62a4681361961b378b6395df67e
qtuancr261 <tuancr261@gmail.com>
2018-04-06 21:51:24 +0700

    Update DataObserver Interface

--- Observer/dataobserver.h
+++ Observer/dataobserver.h
@@ -2,8 +2,15 @@
 #define DATAOBSERVER_H
 #include "Model/coin.h"
 #include <QVector>
+using CoinPtr = shared_ptr<Coin>;
 class DataObserver
 {
-    virtual void update(QVector<Coin*> coins) = 0;
+    //virtual void update(QVector<Coin*> coins) = 0;
+public:
+    virtual const QString& getName() const = 0;
+    virtual void setName(const QString& name) = 0;
+    virtual const QHash<QString, CoinPtr>& getTrackedCoins() const = 0;
+    virtual void addNewTrackedCoin(const QString& coinSymbol, const CoinPtr& coin) = 0;
+    virtual void removeTrackedCoin(const QString& coinSymbol) = 0;
 };
 #endif // DATAOBSERVER_H
