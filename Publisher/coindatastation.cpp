be416a26116acf67c3392201463c4692d9a056b6
qtuancr261 <tuancr261@gmail.com>
2018-04-09 01:13:09 +0700

    improve gotLastValueOfALLCoins(QNetworkReply *replyFromServer) | implement registerObserver(const DataObserverPtr &observer)

--- Publisher/coindatastation.cpp
+++ Publisher/coindatastation.cpp
@@ -33,18 +33,21 @@ void CoinDataStation::gotLastValueOfALLCoins(QNetworkReply *replyFromServer)
     {
         // Parse Coin Object and its last Value
         QJsonObject currentObj{jsonArray.at(0).toArray().at(i).toObject()};
-        QJsonObject currentValue{currentObj.value("last_values").toObject()};
+        QJsonObject lastValue{currentObj.value("last_values").toObject()};
         // Get the value we want from the key we provide
         QString symbol{currentObj.value("symbol").toString()};
         QString name{currentObj.value("name").toString()};
         long availableSupply{currentObj.value("available_supply").toString().toLong()};
 
-        QDateTime time;
-        time.setTime_t(currentValue.value("timeStamp").toInt());
-        qDebug() << time.toString("dd MM yyyy");
         //qDebug() << currentValue.value("price").toDouble() << currentValue.value("timeStamp").toDouble();
         CoinPtr currentCoin{make_shared<Coin>(name, symbol)};
         currentCoin->setAvailable_supply(availableSupply);
+        currentCoin->getLastValue().timeStamp.setTime_t(lastValue.value("timeStamp").toInt());
+        currentCoin->getLastValue().price = lastValue.value("price").toDouble();
+        currentCoin->getLastValue().marketcap = lastValue.value("marketcap").toInt();
+        currentCoin->getLastValue().volume_24h = lastValue.value("volume24h").toDouble();
+        currentCoin->getLastValue().changedPercent_1h = lastValue.value("change_1h").toString();
+        currentCoin->getLastValue().changedPercent_24h = lastValue.value("change_24h").toString();
         trackedCoins.insert(symbol, currentCoin);
         //qDebug() << jsonArray.at(0).toArray().at(i).toObject().value("symbol").toString();
         //
@@ -52,9 +55,24 @@ void CoinDataStation::gotLastValueOfALLCoins(QNetworkReply *replyFromServer)
     emit parseLastValueOfAllCoinsCompleted(trackedCoins);
 }
 
+void CoinDataStation::createANewCollectionWith(const QString &collectionName, const QStringList &contents)
+{
+    CoinCollectionPtr newCollection{make_shared<CoinCollection>(CoinCollection(collectionName))};
+    for (const QString& symbol : contents)
+    {
+        if(trackedCoins.contains(symbol))
+            newCollection->addNewTrackedCoin(symbol, trackedCoins.value(symbol));
+    }
+    registerObserver(newCollection);
+    qDebug() << observers.contains(collectionName);
+    emit creatingANewCollectionCompleted(collectionName);
+}
+
 
 void CoinDataStation::registerObserver(const DataObserverPtr &observer)
 {
+    if (!observers.contains(observer->getName()))
+        observers.insert(observer->getName(), observer);
 }
 
 void CoinDataStation::removeObserver(const DataObserverPtr &observer)
