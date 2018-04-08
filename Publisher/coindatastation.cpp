f4443414ac6601f3b1f9baf32799f96c840a9ea0
qtuancr261 <tuancr261@gmail.com>
2018-04-08 16:21:16 +0700

    test QDateTime with UNIX timestamp

--- Publisher/coindatastation.cpp
+++ Publisher/coindatastation.cpp
@@ -31,12 +31,20 @@ void CoinDataStation::gotLastValueOfALLCoins(QNetworkReply *replyFromServer)
     QJsonArray jsonArray{jsonDoc.array()};
     for (int i{}; i < jsonArray.at(0).toArray().size(); i++)
     {
+        // Parse Coin Object and its last Value
         QJsonObject currentObj{jsonArray.at(0).toArray().at(i).toObject()};
         QJsonObject currentValue{currentObj.value("last_values").toObject()};
+        // Get the value we want from the key we provide
         QString symbol{currentObj.value("symbol").toString()};
         QString name{currentObj.value("name").toString()};
+        long availableSupply{currentObj.value("available_supply").toString().toLong()};
+
+        QDateTime time;
+        time.setTime_t(currentValue.value("timeStamp").toInt());
+        qDebug() << time.toString("dd MM yyyy");
         //qDebug() << currentValue.value("price").toDouble() << currentValue.value("timeStamp").toDouble();
         CoinPtr currentCoin{make_shared<Coin>(name, symbol)};
+        currentCoin->setAvailable_supply(availableSupply);
         trackedCoins.insert(symbol, currentCoin);
         //qDebug() << jsonArray.at(0).toArray().at(i).toObject().value("symbol").toString();
         //
