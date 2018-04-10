3611c7e36dc32d974fbb54628ae55fa74f9d910c
qtuancr261 <tuancr261@gmail.com>
2018-04-10 16:47:00 +0700

    all replies we received now will be mapped with repliesMapper with replyKey made by requestCount + CollectionName + RequestType

--- Publisher/coindatastation.cpp
+++ Publisher/coindatastation.cpp
@@ -17,17 +17,26 @@ QHash<QString, DataObserverPtr> &CoinDataStation::getRefObservers()
 
 CoinDataStation::CoinDataStation(QObject *parent)
     : QObject(parent),
-      networkManager{new QNetworkAccessManager(this)}
+      networkManager{new QNetworkAccessManager(this)},
+      repliesHashTable{},
+      repliesMapper{new QSignalMapper(this)},
+      replyCount{}
 {
-    QObject::connect(networkManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(gotLastValueOfALLCoins(QNetworkReply*)));
+    //QObject::connect(networkManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(gotLastValueOfALLCoins(QNetworkReply*)));
+    QObject::connect(repliesMapper, SIGNAL(mapped(QString)), this , SLOT(replyFromServerArrived(QString)));
 }
 
 void CoinDataStation::getLastValueOfAllCoins()
 {
-    networkManager->get(QNetworkRequest(QUrl(API.getGetDataWithoutAllValues())));
+    QNetworkReply* reply{networkManager->get(QNetworkRequest(API.getLastValueOfAllCoinsUrl()))};
+    QString replyKey{QString("%1|%2|%3").arg(QString::number(replyCount++)).arg("ALL").arg("lastvalue")};
+    repliesHashTable.insert(replyKey, reply);
+    repliesMapper->setMapping(reply, replyKey);
+    QObject::connect(reply, SIGNAL(finished()), repliesMapper, SLOT(map()));
+
 }
 
-void CoinDataStation::gotLastValueOfALLCoins(QNetworkReply *replyFromServer)
+void CoinDataStation::parseLastValueOfALLCoins(QNetworkReply *replyFromServer)
 {
     //qDebug() << replyFromServer->readAll();
     QString jsonString{replyFromServer->readAll()};
@@ -54,8 +63,9 @@ void CoinDataStation::gotLastValueOfALLCoins(QNetworkReply *replyFromServer)
         currentCoin->setAvailable_supply(availableSupply);
         currentCoin->getLastValue().timeStamp.setTime_t(lastValue.value("timeStamp").toInt());
         currentCoin->getLastValue().price = lastValue.value("price").toDouble();
-        currentCoin->getLastValue().marketcap = lastValue.value("marketcap").toInt();
-        currentCoin->getLastValue().volume_24h = lastValue.value("volume24h").toDouble();
+        currentCoin->getLastValue().marketcap = lastValue.value("marketcap").toDouble();
+        currentCoin->getLastValue().volume_24h = lastValue.value("volume24").toString().toDouble();
+        //qDebug() << lastValue.value("volume24").toString();
         currentCoin->getLastValue().changedPercent_1h = lastValue.value("change_1h").toString();
         currentCoin->getLastValue().changedPercent_24h = lastValue.value("change_24h").toString();
         trackedCoins.insert(symbol, currentCoin);
@@ -63,6 +73,54 @@ void CoinDataStation::gotLastValueOfALLCoins(QNetworkReply *replyFromServer)
         //
     }
     emit parseLastValueOfAllCoinsCompleted(trackedCoins);
+}
+
+void CoinDataStation::getMaxValueIn7DaysOfAllCoins()
+{
+
+}
+
+void CoinDataStation::parseMaxValueIn7DaysOfAllCoins(QNetworkReply *replyFromServer)
+{
+    QString jsonString{replyFromServer->readAll()};
+    QJsonDocument jsonDoc{QJsonDocument::fromJson(jsonString.toUtf8())};
+    if (jsonDoc.isEmpty())
+        qDebug() << "Empty";
+    else if (jsonDoc.isObject())
+        qDebug() << "Object";
+    else if (jsonDoc.isArray())
+        qDebug() << "Array";
+    QJsonArray jsonArray{jsonDoc.array()};
+    for (int i{}; i < jsonArray.at(0).toArray().size(); i++)
+    {
+        // Parse Coin Object and its last Value
+        QJsonObject currentObj{jsonArray.at(0).toArray().at(i).toObject()};
+        QJsonObject lastValue{currentObj.value("last_values").toObject()};
+        // Get the value we want from the key we provide
+        QString symbol{currentObj.value("symbol").toString()};
+        QString name{currentObj.value("name").toString()};
+        long availableSupply{currentObj.value("available_supply").toString().toLong()};
+
+        //qDebug() << currentValue.value("price").toDouble() << currentValue.value("timeStamp").toDouble();
+        CoinPtr currentCoin{make_shared<Coin>(name, symbol)};
+        currentCoin->setAvailable_supply(availableSupply);
+        currentCoin->getLastValue().timeStamp.setTime_t(lastValue.value("timeStamp").toInt());
+        currentCoin->getLastValue().price = lastValue.value("price").toDouble();
+        currentCoin->getLastValue().marketcap = lastValue.value("marketcap").toDouble();
+        currentCoin->getLastValue().volume_24h = lastValue.value("volume24").toString().toDouble();
+        //qDebug() << lastValue.value("volume24").toString();
+        currentCoin->getLastValue().changedPercent_1h = lastValue.value("change_1h").toString();
+        currentCoin->getLastValue().changedPercent_24h = lastValue.value("change_24h").toString();
+        trackedCoins.insert(symbol, currentCoin);
+        //qDebug() << jsonArray.at(0).toArray().at(i).toObject().value("symbol").toString();
+        //
+    }
+    emit parseMaxValueIn7DaysOfAllCoinsCompleted(trackedCoins);
+}
+
+void CoinDataStation::replyFromServerArrived(const QString& replyDiscription)
+{
+    parseLastValueOfALLCoins(repliesHashTable.value(replyDiscription));
 }
 
 void CoinDataStation::createANewCollectionWith(const QString &collectionName, const QStringList &contents)
