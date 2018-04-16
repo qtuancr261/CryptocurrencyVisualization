b046ae374432bdc7d539b7f9ada5016ffefc6d3c
qtuancr261 <tuancr261@gmail.com>
2018-04-16 10:58:20 +0700

    change to dev-1 branch

--- main.cpp
+++ main.cpp
@@ -15,40 +15,7 @@ int main(int argc, char *argv[])
     splashScreen.showMessage("Updating From Server......", Qt::AlignBottom | Qt::AlignLeft, Qt::white);
     //a.processEvents();
     MainWindow w;
-
-    /*QLineSeries *series = new QLineSeries();
-    series->setName("BTC");
-    *series << QPointF(10.0, 7000.0) << QPointF(12.0, 7325.0) << QPointF(13.0, 6808.0) << QPointF(14.0, 8200.0)
-            << QPointF(15.0, 4325.0) << QPointF(16.0, 1358.0);
-    QLineSeries *series_2 = new QLineSeries();
-
-    series_2->setName("ETH");
-    *series_2 << QPointF(10.0, 7520.0) << QPointF(12.0, 7025.0) << QPointF(13.0, 9808.0) << QPointF(14.0, 7853.09)
-            << QPointF(15.0, 4325.0) << QPointF(16.0, 4520.06);
-    //![1]
-
-    //![2]
-    QChart *chart = new QChart();
-    chart->addSeries(series);
-    chart->addSeries(series_2);
-    //chart->legend()->hide();
-    chart->setTitle("Price Tracking Chart");
-    //![2]
-
-    //![3]
-    QValueAxis *axisX = new QValueAxis();
-    axisX->setTitleText("Time Stamp");
-    axisX->setLabelFormat("%i");
-    axisX->setTickCount(series->count());
-    chart->addAxis(axisX, Qt::AlignBottom);
-    series->attachAxis(axisX);
-
-    QValueAxis *axisY = new QValueAxis();
-    axisY->setTitleText("Price");
-    axisY->setLabelFormat("%i");
-    axisY->setTickCount(series->count());
-    chart->addAxis(axisY, Qt::AlignLeft);
-    series->attachAxis(axisY);
+	// CFFFRRRR
     //![3]*/
     /*QBarSet* btcSet{new QBarSet("BTC")};
     QBarSet* ethSet{new QBarSet("ETH")};
