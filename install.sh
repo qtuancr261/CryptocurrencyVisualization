5c5f300874f14ea2adb73045bf7441d457c0bbb2
tuantq3 <tuantq3@vng.com.vn>
2019-05-04 07:51:47 +0700

    Inform qmake spec: compiler

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Welcome to CryptoConcurrency Visualization 
+#
+echo "You can config the compiler for qmake to use by command: qmake -spec <compile-spec>"
+echo "qmake specs could be found in /usr/lib/x86_64-linux-gnu/qt5/mkspecs"
 echo "CryptoConcurrency Visualization v1.0"
 qmake
 make
